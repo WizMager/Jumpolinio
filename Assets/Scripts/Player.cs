734edb276e985571ed49f818029c2a935564c41c
WizMager <wizmager@gmail.com>
2022-09-24 20:44:16 +0300

    feat: create idle animation and walk left feat: create animator controller and add change state feat: add player instantiate and injection in Zenject(first test)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Camera mainCamera;
     [SerializeField] private float playerMoveSpeed;
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private CapsuleCollider2D playerCollider;
+    [SerializeField] private BoxCollider2D playerCollider;
     [SerializeField] private Rigidbody2D playerRigidbody;
     [SerializeField] private float jumpForce;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private bool _spriteRendererFlip;
     private Controls _controls;
 
     private void Awake()
@@ -35,10 +38,32 @@ public class Player : MonoBehaviour
 
     private void PlayerMove()
     {
+        animator.SetBool("Walk", false);
         if (_controls.TouchInput.Move.phase != InputActionPhase.Performed) return;
-        playerTransform.Translate( playerTransform.right * _controls.TouchInput.Move.ReadValue<float>() * playerMoveSpeed * Time.deltaTime);
+        var axisXInputValue = _controls.TouchInput.Move.ReadValue<float>();
+        animator.SetBool("Walk", true);
+        animator.SetFloat("WalkDirection", axisXInputValue);
+        FlipSpriteToMoveDirection(axisXInputValue);
+        playerTransform.Translate( playerTransform.right * axisXInputValue * playerMoveSpeed * Time.deltaTime);
     }
 
+    private void FlipSpriteToMoveDirection(float axisXInputValue)
+    {
+        if (axisXInputValue < 0)
+        {
+            if (_spriteRendererFlip) return;
+            _spriteRendererFlip = true;
+            spriteRenderer.flipX = _spriteRendererFlip;
+        }
+        
+        if (axisXInputValue > 0)
+        {
+            if (!_spriteRendererFlip)return;
+            _spriteRendererFlip = false;
+            spriteRenderer.flipX = _spriteRendererFlip;
+        } 
+    }
+    
     private void OnDisable()
     {
         _controls.TouchInput.Disable();
