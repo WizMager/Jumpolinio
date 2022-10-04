17ee631f963c19d16da6f9b0850ac5fb0dfbbc37
WizMager <wizmager@gmail.com>
2022-10-04 13:52:40 +0300

    feat: add animation for mining crystal(with rotate to crystal)

--- Assets/Scripts/MonoController/ControllerScripts/PlayerShoot.cs
+++ Assets/Scripts/MonoController/ControllerScripts/PlayerShoot.cs
@@ -14,6 +14,7 @@ namespace MonoController.ControllerScripts
         private Vector3 _targetPosition;
         private readonly LineRenderer _lineRenderer;
         private readonly PortalView _portalView;
+        private readonly Animator _animator;
 
         public PlayerShoot(List<CrystalView> crystalViews, PlayerComponents playerComponents, AllPlatformsComponents allPlatformsComponents)
         {
@@ -21,6 +22,7 @@ namespace MonoController.ControllerScripts
             _shootPosition = playerComponents.GetShootPosition;
             _lineRenderer = playerComponents.GetLineRenderer;
             _portalView = allPlatformsComponents.GetPortalView;
+            _animator = playerComponents.GetAnimator;
         }
 
         private void CrystalSubscription(List<CrystalView> crystalViews, bool sub)
@@ -43,6 +45,7 @@ namespace MonoController.ControllerScripts
 
         private void OnCrystalDestroyHandler(CrystalView currentCrystal)
         {
+            _animator.SetBool("Mine", false);
             _lineRenderer.enabled = false;
             currentCrystal.OnPlayerEnterShootRange -= OnShootRangeEnterHandler;
             currentCrystal.OnPlayerDestroyCrystal -= OnCrystalDestroyHandler;
@@ -57,7 +60,9 @@ namespace MonoController.ControllerScripts
         {
             _shoot = inside;
             _lineRenderer.enabled = false;
+            _animator.SetBool("Mine", false);
             if (!inside) return;
+            _animator.SetBool("Mine", true);
             _targetPosition = position;
             _lineRenderer.enabled = true;
         }
@@ -66,6 +71,8 @@ namespace MonoController.ControllerScripts
         public void Update(float deltaTime)
         {
             if (!_shoot) return;
+            var axisXInputValue = _shootPosition.position.x - _targetPosition.x;
+            _animator.SetBool("RightMineDirection", axisXInputValue < 0);
             Vector3[] linePositions = {_shootPosition.position, _targetPosition};
             _lineRenderer.SetPositions(linePositions);
         }
